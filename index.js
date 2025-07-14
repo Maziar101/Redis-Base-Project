2c223aa68916b9fdc7bd98744e0d30021b68c625
Maziar <maziarzamani85@gmail.com>
2025-07-14 17:47:31 +0330

    Add Routes To Index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import userRouter from "./User/Routes/index.js";
+import orderRouter from "./Order/Routes/index.js";
+
+const app = express();
+const port = 5000;
+
+app.get(`/user`,userRouter);
+
+app.get("/order",orderRouter);
+
+app.listen(port, () => {
+  console.log(`SERVER IS RUNNING ON PORT ${port}`);
+});
+
+export default app;
