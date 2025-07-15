4fe0f9e7ccb3b840b311c749dd1fb1ccd115d633
Maziar <maziarzamani85@gmail.com>
2025-07-15 12:59:48 +0330

    Create HandleCache
    Set And Get

---
+++ utils/handleCache.js
@@ -0,0 +1,9 @@
+import client from "../redis.js";
+
+export const setCache = async (key, second, value) => {
+  await client.setEx(key, second, JSON.stringify(value));
+};
+
+export const getCache = async (key) => {
+  return await JSON.parse(client.get(key));
+};
